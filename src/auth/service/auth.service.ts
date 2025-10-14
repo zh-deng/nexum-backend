54ad14a2cce5e67a0374d9c7e4fb035057bd7aab
zh-deng <heromemo@web.de>
2025-10-14 13:42:29 +0200

    feat(auth): add auth service

---
+++ src/auth/service/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService
+  ) {}
+
+  // Handles user registration
+  async signup(email: string, userName: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    return this.prisma.user.create({
+      data: {
+        email,
+        userName,
+        password: hashedPassword,
+      },
+    });
+  }
+
+  // Handles user login and token generation
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) throw new Error('Invalid credentials');
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) throw new Error('Invalid credentials');
+
+    // Create token with userId as the subject (identifier)
+    const token = this.jwt.sign({
+      sub: user.id,
+      email: user.email,
+    });
+
+    return { acccess_token: token };
+  }
+}
