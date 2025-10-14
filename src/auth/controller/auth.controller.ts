044d2ac5d88fdcf91c42b1206fc654c54a5d0337
zh-deng <heromemo@web.de>
2025-10-14 14:00:31 +0200

    feat(auth): add signup and login endpoints in auth controller

---
+++ src/auth/controller/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from '../service/auth.service';
+import { SignupDto } from '../dto/signup.dto';
+import { LoginDto } from '../dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() dto: SignupDto) {
+    return this.authService.signup(dto.email, dto.userName, dto.password)
+  }
+
+  @Post('login')
+  async login(@Body() dto: LoginDto) {
+    return this.authService.login(dto.email, dto.password)
+  }
+}
