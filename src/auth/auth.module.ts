1499e0688095f62950f1f61919b0611f17e91341
zh-deng <heromemo@web.de>
2025-10-14 13:42:29 +0200

    feat(auth): add auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './service/auth.service';
 import { AuthController } from './controller/auth.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
