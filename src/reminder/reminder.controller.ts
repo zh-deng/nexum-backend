136b74b7c7a742b0be80683b7c8c9fec7898970e
zh-deng <heromemo@web.de>
2025-10-15 21:16:50 +0200

    feat(reminder): add module, controller and service

---
+++ src/reminder/reminder.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ReminderService } from './reminder.service';
+import { CreateReminderDto } from './dtos/create-reminder.dto';
+
+@Controller('reminders')
+export class ReminderController {
+  constructor(private readonly reminderService: ReminderService) {}
+
+  @Post()
+  create(@Body() dto: CreateReminderDto) {
+    return this.reminderService.create(dto);
+  }
+
+  @Get()
+  findAll(@Body() applicationId: string) {
+    return this.reminderService.findAll(applicationId);
+  }
+}
