6c38913795ce093b8eeddad382384bc9986a5baa
zh-deng <heromemo@web.de>
2025-10-15 20:54:00 +0200

    feat(company): add create and update dto

---
+++ src/company/dtos/create-company.dto.ts
@@ -0,0 +1,58 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateCompanyDto {
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  website?: string;
+
+  @IsOptional()
+  @IsString()
+  street?: string;
+
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @IsOptional()
+  @IsString()
+  state?: string;
+
+  @IsOptional()
+  @IsString()
+  zipCode?: string;
+
+  @IsOptional()
+  @IsString()
+  country?: string;
+
+  @IsOptional()
+  @IsString()
+  industry?: string;
+
+  @IsOptional()
+  @IsString()
+  companySize?: string;
+
+  @IsOptional()
+  @IsString()
+  contactName?: string;
+
+  @IsOptional()
+  @IsString()
+  contactEmail?: string;
+
+  @IsOptional()
+  @IsString()
+  contactPhone?: string;
+
+  @IsOptional()
+  @IsString()
+  notes?: string;
+
+  @IsOptional()
+  @IsString()
+  logoUrl?: string;
+}
