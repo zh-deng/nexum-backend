66c50ac9c86e05e9fa74aa131ec421cce818e9f6
zh-deng <heromemo@web.de>
2025-10-15 21:13:24 +0200

    feat(log-item): add module, controller and service

---
+++ src/log-item/log-item.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { LogItemService } from './log-item.service';
+import { CreateLogItemDto } from './dtos/create-log-item.dto';
+import { UpdateLogItemDto } from './dtos/update-log-item.dto';
+
+@Controller('log-items')
+export class LogItemController {
+  constructor(private readonly logItemService: LogItemService) {}
+
+  @Post()
+  create(@Body() dto: CreateLogItemDto) {
+    return this.logItemService.create(dto);
+  }
+
+  @Patch()
+  update(@Param() logItemId: string, dto: UpdateLogItemDto) {
+    return this.logItemService.update(logItemId, dto);
+  }
+
+  @Get()
+  findAll(@Body() applicationId: string) {
+    return this.logItemService.findAll(applicationId);
+  }
+}
