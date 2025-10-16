77ac258ab8f94f426ce761e39b8f6383a0bf1d51
zh-deng <heromemo@web.de>
2025-10-16 12:06:48 +0200

    feat(user): add create and update dto

---
+++ src/user/dtos/update-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsOptional, IsString, MinLength } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  username?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  password?: string;
+}
