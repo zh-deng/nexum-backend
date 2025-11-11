f3870adf912fb4c10adbe6e26035f9f8281c345b
zh-deng <heromemo@web.de>
2025-11-11 17:46:34 +0100

    refactor: upgrade prisma module to global module

--- src/chart/chart.module.ts
+++ src/chart/chart.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
-import { PrismaService } from "../../prisma/prisma.service";
 import { ChartController } from "./chart.controller";
 import { ChartService } from "./chart.service";
 
 @Module({
   controllers: [ChartController],
-  providers: [ChartService, PrismaService]
+  providers: [ChartService]
 })
 
 export class ChartModule {
--- src/reminder/reminder.module.ts
+++ src/reminder/reminder.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common";
-import { ReminderController } from "./reminder.controller";
-import { ReminderService } from "./reminder.service";
-import { PrismaService } from "../../prisma/prisma.service";
+import { Module } from '@nestjs/common';
+import { ReminderController } from './reminder.controller';
+import { ReminderService } from './reminder.service';
+import { QueuesModule } from '../queues/queues.module';
 
 @Module({
+  imports: [QueuesModule],
   controllers: [ReminderController],
-  providers: [ReminderService, PrismaService]
+  providers: [ReminderService],
+  exports: [ReminderService]
 })
-
 export class ReminderModule {}
