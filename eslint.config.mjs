af082812360ed447fd5aaad0a1a6aa8db3e49d1b
zh-deng <heromemo@web.de>
2025-11-19 20:44:45 +0100

    chore: update docker compose and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,10 +25,14 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
-  prettier,
+  prettier
 );
